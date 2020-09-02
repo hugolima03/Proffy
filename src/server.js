6c2da33569c4e77a15852448c0ff18a470e1b037
HugoLima <hugo8romao@gmail.com>
2020-09-02 14:50:25 -0400

    ajustando caminhos de imagens da index.html

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
-require('express')()
+const express = require('express');
+const server = express();
+
+server.use(express.static("public"))
 .get("/", (req, res) => {
-  return res.send("Hi from Hugo")
+  return res.sendFile(__dirname + "/views/index.html")
 })
 .get("/study", (req, res) => {
   return res.send("Study")
