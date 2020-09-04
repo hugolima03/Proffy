4332d376f9411f3e892a986a507b1e23fa47e428
HugoLima <hugo8romao@gmail.com>
2020-09-04 15:27:39 -0400

    linkando database com o submit

--- src/server.js
+++ src/server.js
@@ -5,7 +5,8 @@ const server = express();
 const { 
   pageLanding, 
   pageStudy, 
-  pageGiveClasses 
+  pageGiveClasses,
+  saveClasses
 } = require('./pages')
 
 
@@ -18,11 +19,14 @@ nunjucks.configure('src/views', {
 
 //Configurando arquivos estáticos (css, scripts, imagens)
 server
+// receber os dados do req.body
+.use(express.urlencoded({ extended: true }))
 // configurar arquivos estáticos (css, scripts, imagens)
 .use(express.static("public"))
 //Rotas da aplicação
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
 //start do servidor
 .listen(5500);
